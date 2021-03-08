3d2ce2f5279f03fbaf50eecaee4e2cea2a925047
Benjamin Benson <bensonb@mail.gvsu.edu>
2021-03-08 17:10:12 -0500

    working on setting up the project also, a better readme

---
+++ solid-octo-couscous/apps/rex/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'solid-octo-couscous-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'rex';
+}
