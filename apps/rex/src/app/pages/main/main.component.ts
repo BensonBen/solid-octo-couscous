61866a4e2931f1bc2fccfcd588705c4a5e5894a6
Benjamin Benson <bbenson@promodel.com>
2021-05-20 22:13:39 -0400

    #28 fix proxy configuration issue for local development finally

--- apps/rex/src/app/pages/main/main.component.ts
+++ apps/rex/src/app/pages/main/main.component.ts
@@ -1,12 +1,8 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
 	selector: 'solid-octo-couscous-main',
 	template: `<h2>hey bik boi</h2>`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainComponent implements OnInit {
-	ngOnInit(): void {
-		console.log('I am here');
-	}
-}
+export class MainComponent {}
