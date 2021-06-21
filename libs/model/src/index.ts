97d6559a7637e5d3c0c9f6aa84a2f5041fa6f832
Benjamin Benson <bensonb@mail.gvsu.edu>
2021-06-20 22:10:32 -0700

    5 setup connection to heartrate monitor (#39)
    #5 preliminary work prior to working more with web bluetooth

--- libs/model/src/index.ts
+++ libs/model/src/index.ts
@@ -1,6 +1,9 @@
 export { User } from './lib/model/user';
+export { Unsubscriber } from './lib/util/unsubscriber';
 export { SocAuthenticationConfiguration } from './lib/util/soc-authentication-configuration';
-export { Transaction } from './lib/reponses/transaction';
+export { Transaction } from './lib/responses/transaction';
 export { NewUserRequest } from './lib/requests/new-user-request';
-export { LoginUserResponse } from './lib/reponses/login-user-response';
+export { LoginUserResponse } from './lib/responses/login-user-response';
 export { LoginUserRequest } from './lib/requests/login-user-request';
+export { SchwinIc4BluetoothCharacteristics } from './lib/model/schwin-ic4-bluetooth-characteristics';
+export { SchwinIc4BluetoothServices } from './lib/model/schwin-ic4-bluetooth-services';
