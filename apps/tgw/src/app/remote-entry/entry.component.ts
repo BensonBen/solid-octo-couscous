4014acff9a64490b39af577c5f0038759a81ef75
Benjamin Benson <bensonb@mail.gvsu.edu>
2021-10-11 23:11:34 -0400

    #35 fix linting error

--- apps/tgw/src/app/remote-entry/entry.component.ts
+++ apps/tgw/src/app/remote-entry/entry.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'solid-octo-couscous-tgw-entry',
+	selector: 'tgw-solid-octo-couscous-entry',
 	template: `<div class="remote-entry"><h2>tgw's Remote Entry Component</h2></div>`,
 	styles: [
 		`
