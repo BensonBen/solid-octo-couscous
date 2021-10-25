3afeb395581030a52a895743dbbcf4ef549bc26e
Benjamin Benson <bensonb@mail.gvsu.edu>
2021-10-24 18:47:50 -0700

    Fix toast messages (#82)
    #72 fix toast messages for primary and error stylings

--- apps/rex/src/app/app.component.spec.ts
+++ apps/rex/src/app/app.component.spec.ts
@@ -2,11 +2,12 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('AppComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			imports: [RouterTestingModule],
+			imports: [RouterTestingModule, MatSnackBarModule],
 			declarations: [AppComponent],
 			schemas: [NO_ERRORS_SCHEMA],
 		}).compileComponents();
