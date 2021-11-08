6c96aab3a9fb8bc7e47d9cd76cc7acc4f61352bd
Benjamin Benson <bensonb@mail.gvsu.edu>
2021-11-08 00:40:24 -0500

    upgrade to angular 13

---
+++ apps/rex-api/src/v1/tensorflow-js/auth-service.ts
@@ -0,0 +1,46 @@
+import { NewUserRequest, User, LoginUserResponse } from '@solid-octo-couscous/model';
+import { green } from 'chalk';
+import { inject, singleton } from 'tsyringe';
+import { AuthDataProvider } from './auth-data-provider';
+import { omit as _omit } from 'lodash';
+import { sign as jwtSign, Algorithm, SignOptions } from 'jsonwebtoken';
+
+@singleton()
+export class TensorFlowService {
+	private readonly logger = console;
+	private readonly loggerPrefix = '[TensorFlowService]';
+	private readonly jwtSignOptions: SignOptions = {
+		algorithm: process?.env?.AUTH_API_JWT_ALG as Algorithm,
+		audience: process?.env?.AUTH_API_JWT_AUDIENCE,
+		issuer: process?.env?.AUTH_API_JWT_ISSUER,
+		expiresIn: '1h',
+	};
+
+	constructor(@inject(AuthDataProvider) private readonly authDataProvider?: AuthDataProvider) {}
+
+	/**
+	 * Handles the POST request for creating a users account.
+	 *
+	 * @param Request, body the body of the request from a POST request.
+	 * @param response, response the response object on which to attach objects.
+	 * @returns, a Promise that stores the Transactional data from the new account created.
+	 */
+	public readonly createAccount = async (userRequest: Readonly<NewUserRequest>): Promise<LoginUserResponse> => {
+		// prevent logging of sensitive information.
+		this.logger.log(
+			green(
+				`${this.loggerPrefix} Attempting to create new user account using: ${JSON.stringify({
+					...userRequest,
+					password: null,
+					email: null,
+				})}`
+			)
+		);
+
+		const omission: keyof User = 'password';
+		const user: LoginUserResponse = _omit(await this.authDataProvider.createAccount(userRequest), omission);
+		const jwtToken = jwtSign(user, process?.env?.AUTH_API_JWT_KEY, this.jwtSignOptions);
+
+		return { ...user, jwtToken } as LoginUserResponse;
+	};
+}
